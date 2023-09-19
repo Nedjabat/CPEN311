04aa589ffc5fc62361c6959e1ba419c357526c94
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2023-09-18 20:58:45 -0700

    reg4 test bench good

--- Lab-1/Lab-1/task5/scorehand.sv
+++ Lab-1/Lab-1/task5/scorehand.sv
@@ -1,6 +1,7 @@
 module scorehand(input [3:0] card1, input [3:0] card2, input [3:0] card3, output [3:0] total);
 
     assign total = (card1+card2+card3)%10;
+    
 // The code describing scorehand will go here.  Remember this is a combinational
 // block. The function is described in the handout.  Be sure to review the section
 // on representing numbers in the lecture notes.
--- Lab-1/Lab-1/task5/tb_reg4.sv
+++ Lab-1/Lab-1/task5/tb_reg4.sv
@@ -1,1 +1,51 @@
+`timescale 1 ps / 1 ps
 
+module tb_reg4;
+    reg tb_D;
+    reg tb_clk;
+    reg tb_reset;
+    wire tb_Q;
+
+    reg4 DUT (.D(tb_D), .clk(tb_clk), .reset(tb_reset), .Q(tb_Q));
+
+    always begin
+        tb_clk = 1;
+        tb_D = 1;
+        tb_reset = 1;
+        #10;
+        tb_clk = 0;
+        #10;
+        tb_clk = 1;
+        tb_D = 0;
+        tb_reset = 1;
+        #10;
+        tb_clk = 0;
+        #10;
+        tb_clk = 1;
+        tb_D = 1;
+        tb_reset = 0;
+        #10;
+        tb_clk = 0;
+        #10;
+        tb_clk = 1;
+        tb_D = 1;
+        tb_reset = 1;
+        #10;
+        tb_clk = 0;
+        #10;
+        tb_clk = 1;
+        tb_D = 0;
+        tb_reset = 1;
+        #10;
+        tb_clk = 0;
+        #10;
+        tb_clk = 1;
+        tb_D = 1;
+        tb_reset = 0;
+        #10;
+        tb_clk = 0;
+        #10;
+    end
+
+
+endmodule  
