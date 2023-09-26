28bc7dd076468316edbc1a3d44099379ad032daa
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2023-09-25 22:21:17 -0700

    fixed some stuff

--- Lab-1/Lab-1/task5/statemachine.sv
+++ Lab-1/Lab-1/task5/statemachine.sv
@@ -1,10 +1,10 @@
 module statemachine(input slow_clock, input resetb,
                     input [3:0] dscore, input [3:0] pscore, input [3:0] pcard3,
-                    output load_pcard1, output load_pcard2,output load_pcard3,
-                    output load_dcard1, output load_dcard2, output load_dcard3,
-                    output player_win_light, output dealer_win_light);
+                    output logic load_pcard1, output logic load_pcard2,output logic load_pcard3,
+                    output logic load_dcard1, output logic load_dcard2, output logic load_dcard3,
+                    output logic player_win_light, output logic dealer_win_light);
 
-int state, S0, S1, S2, S3, S4, S5, S6, S7, gameover, next_state
+int state, S0, S1, S2, S3, S4, S5, S6, S7, gameover, next_state;
 
 always_comb 
 begin : next_state_logic
@@ -14,24 +14,25 @@ begin : next_state_logic
         S2: next_state = S3;
         S3: next_state = S4;
         S4: if (gameover == 1)
-                next_state = S7
+                next_state = S7;
             else
             begin 
-            if (pcard3 != x)
+            if (pcard3 !== 4'bxxxx)
                 next_state = S5;
             else
-                next_state = S6
+                next_state = S6;                    
             end
         S5: next_state = S7;
         S6: next_state = S7;
-        default: 
+        default: next_state = S0;
     endcase 
 end
 
-always_comb begin : 
+always_comb 
+begin : changes_within_State
     case (state)
         S0:  //P card 1
-        begin 
+        begin                   
             load_pcard1 = 1;
             load_pcard2 = 0;
             load_pcard3 = 0;
@@ -61,13 +62,13 @@ always_comb begin :
             if ((pscore == 8) || (pscore == 9) || (dscore == 8) || (dscore == 9)) //Check for natural
             begin
                 if(pscore > dscore)
-                    player_win_light == 1:
+                    player_win_light = 1;
                 else if (dscore > pscore)
-                    dealer_win_light == 1:
+                    dealer_win_light = 1;
                 else
                 begin 
-                    player_win_light == 1; 
-                    dealer_win_light == 1;
+                    player_win_light = 1; 
+                    dealer_win_light = 1;
                 end
                 gameover = 1;
             end
@@ -90,22 +91,22 @@ always_comb begin :
                 load_pcard3 = 0;
                 load_dcard3 = 1;
             end
-            else if (dscore == 6 and (pcard3 == 6 || pcard3 == 7))
+            else if (dscore == 6 && (pcard3 == 6 || pcard3 == 7))
             begin
                 load_pcard3 = 0;
                 load_dcard3 = 1;
             end
-            else if (dscore == 5 and (pcard3 > 3 || pcard3 < 8))
+            else if (dscore == 5 && (pcard3 > 3 || pcard3 < 8))
             begin
                 load_pcard3 = 0;
                 load_dcard3 = 1;
             end
-            else if (dscore == 4 and (pcard3 > 1 || pcard3 < 8))
+            else if (dscore == 4 && (pcard3 > 1 || pcard3 < 8))
             begin
                 load_pcard3 = 0;
                 load_dcard3 = 1;
             end
-            else if (dscore == 3 and pcard3 !== 8)
+            else if (dscore == 3 && pcard3 !== 8)
             begin
                 load_pcard3 = 0;
                 load_dcard3 = 1;
@@ -132,23 +133,23 @@ always_comb begin :
         S7:
             begin
                 if(pscore > dscore)
-                    player_win_light == 1:
+                    player_win_light = 1;
                 else if (dscore > pscore)
-                    dealer_win_light == 1:
+                    dealer_win_light = 1;
                 else
                 begin 
-                    player_win_light == 1; 
-                    dealer_win_light == 1;
+                    player_win_light = 1; 
+                    dealer_win_light = 1;
                 end
             end
-        default: player_win_light == 1; 
+        default: player_win_light = 1; 
 
     endcase
 end
 
-always_ff @(negedge rst, posedge slow_clock ) 
+always_ff @(negedge resetb, posedge slow_clock ) 
 begin
-    if(~reset)
+    if(~resetb)
 		state <= S0;
 	else
 		state <= next_state;
