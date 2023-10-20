0c71f5ba35c9f0d9545ca6d6ff4d9686f8399419
zoeyzhao57 <113090188+zoeyzhao57@users.noreply.github.com>
2023-10-20 11:04:04 -0700

    Add files via upload

--- Lab-3/Lab-3-CPEN311/task1/s_mem.v
+++ Lab-3/Lab-3-CPEN311/task1/s_mem.v
@@ -170,5 +170,5 @@ endmodule
 // Retrieval info: GEN_FILE: TYPE_NORMAL s_mem.cmp FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL s_mem.bsf FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL s_mem_inst.v FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL s_mem_bb.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL s_mem_bb.v FALSE
 // Retrieval info: LIB_FILE: altera_mf
