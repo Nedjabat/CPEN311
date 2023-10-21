bd6469cb501e9b210a8a4868e0285d19cdfa1479
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2023-10-21 13:47:40 -0700

    zip

--- Lab-3/Lab-3-CPEN311/task2/tb_syn_task2.sv
+++ Lab-3/Lab-3-CPEN311/task2/tb_syn_task2.sv
@@ -1,6 +1,44 @@
 `timescale 1ps / 1ps
 module tb_syn_task2();
 
-// Your testbench goes here.
+logic CLOCK_50;
+logic [3:0] KEY;
+logic [9:0] SW;
+
+logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+logic [9:0] LEDR;
+
+task2 DUT (	.CLOCK_50, 
+		.KEY, 
+		.SW,
+		.HEX0, 
+		.HEX1,
+		.HEX2,
+		.HEX3,
+		.HEX4,
+		.HEX5,
+		.LEDR);
+
+
+initial begin
+	SW = 10'h33C;
+	CLOCK_50 = 0;  
+	KEY[3] = 1;
+
+	#5;
+
+	KEY[3] = 0;
+
+	#5;
+
+	KEY[3] = 1;
+
+	#5;
+
+
+
+end
+
+initial forever #5 CLOCK_50 = ~CLOCK_50;
 
 endmodule: tb_syn_task2
