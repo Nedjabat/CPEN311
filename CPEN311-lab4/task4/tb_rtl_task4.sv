94783f9031bd2addbd501a60816a6c843f53ccf1
zoeyzhao57 <113090188+zoeyzhao57@users.noreply.github.com>
2023-11-04 16:44:25 -0700

    Add files via upload

--- CPEN311-lab4/task4/tb_rtl_task4.sv
+++ CPEN311-lab4/task4/tb_rtl_task4.sv
@@ -1,5 +1,53 @@
+`timescale 1ps / 1ps
 module tb_rtl_task4();
 
-// Your testbench goes here. Our toplevel will give up after 1,000,000 ticks.
+logic CLOCK_50;
+logic [3:0] KEY;
+logic [9:0] SW;
+
+logic [9:0] LEDR;
+logic [6:0] HEX0; 
+logic [6:0] HEX1;
+logic [6:0] HEX2;
+logic [6:0] HEX3; 
+logic [6:0] HEX4;
+logic [6:0] HEX5;
+logic [7:0] VGA_R;
+logic [7:0] VGA_G;
+logic [7:0] VGA_B;
+logic VGA_HS;
+logic VGA_VS;
+logic VGA_CLK;
+logic [7:0] VGA_X;
+logic [6:0] VGA_Y;
+logic [2:0] VGA_COLOUR;
+logic VGA_PLOT;
+
+//de1_gui gui(.SW, .KEY, .LEDR, .HEX5, .HEX4, .HEX3, .HEX2, .HEX1, .HEX0);
+
+task4 DUT (.CLOCK_50, .KEY,
+             .SW, .LEDR, .HEX0, .HEX1, .HEX2,
+             .HEX3, .HEX4, .HEX5, .VGA_R, .VGA_G, .VGA_B,
+             .VGA_HS, .VGA_VS, .VGA_CLK, .VGA_X, .VGA_Y,
+             .VGA_COLOUR, .VGA_PLOT);
+
+
+
+initial 
+begin
+
+CLOCK_50 = 0;
+#20000;
+KEY[3] = 1;
+#20000;
+KEY[3] = 0;
+#20000;
+KEY[3] = 1;
+
+
+
+end
+
+initial forever #10000 CLOCK_50 = ~CLOCK_50;
 
 endmodule: tb_rtl_task4
