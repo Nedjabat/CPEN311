69117d2aa7bc8f0b672243e0907ba0849a9f6d3f
zoeyzhao57 <113090188+zoeyzhao57@users.noreply.github.com>
2023-11-05 21:47:33 -0800

    Update tb_rtl_fillscreen.sv

--- CPEN311-lab4/task2/tb_rtl_fillscreen.sv
+++ CPEN311-lab4/task2/tb_rtl_fillscreen.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ns
+`timescale 1ps / 1ps
 module tb_rtl_fillscreen();
 
 reg clk;
@@ -12,12 +12,15 @@ wire [6:0] vga_y;
 wire [2:0] vga_colour;
 wire vga_plot;
 
+reg [6:0] counter;
+
 fillscreen DUT(	.clk, .rst_n, .colour, .start,.done,.vga_x,.vga_y,.vga_colour, .vga_plot);
 
 initial 
 begin
 
 clk = 0;
+counter = 0;
 rst_n = 1;
 start = 0;
 #5;
@@ -25,6 +28,43 @@ rst_n = 0;
 start = 1;
 #10;
 rst_n = 1;
+
+#25;
+
+	if(vga_plot == 1)begin
+		counter = counter + 1'b1;
+		$display ("PASSED plot on test");
+	end else begin
+		$error ("FAILED plot on test");
+	end
+
+	if(vga_x == 0)begin
+		counter = counter + 1'b1;
+		$display ("PASSED x test");
+	end else begin
+		$error ("FAILED x test");
+	end
+
+	if(vga_y == 1)begin
+		counter = counter + 1'b1;
+		$display ("PASSED y test");
+	end else begin
+		$error ("FAILED y test");
+	end
+
+#192020;
+
+
+	if(done == 1)begin
+		counter = counter + 1'b1;
+		$display ("PASSED done test");
+	end else begin
+		$error ("FAILED done test");
+	end
+
+
+	$display("%d tests passed out of %d tests.", counter, 6'd4);
+
 
 end
 
