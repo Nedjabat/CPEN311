582799d5b3ec856fc5ea625278be4ccd6d1d1cea
zoeyzhao57 <113090188+zoeyzhao57@users.noreply.github.com>
2023-11-27 05:58:06 -0800

    Add files via upload

--- CPEN311-lab5/task5/music.sv
+++ CPEN311-lab5/task5/music.sv
@@ -1,5 +1,5 @@
 module music(input CLOCK_50, input CLOCK2_50, input [3:0] KEY, input [9:0] SW,
-             input AUD_DACLRCK, input AUD_ADCLRCK, input AUD_BCLK, input AUD_ADCDAT,
+             input AUD_DACLRCK, input AUD_ADCLRCK, input AUD_BCLK, input AUD_ADCDAT, input write_ready,
              inout FPGA_I2C_SDAT, output FPGA_I2C_SCLK, output AUD_DACDAT, output AUD_XCK,
              output [6:0] HEX0, output [6:0] HEX1, output [6:0] HEX2,
              output [6:0] HEX3, output [6:0] HEX4, output [6:0] HEX5,
@@ -8,7 +8,8 @@ module music(input CLOCK_50, input CLOCK2_50, input [3:0] KEY, input [9:0] SW,
 // signals that are used to communicate with the audio core
 // DO NOT alter these -- we will use them to test your design
 
-reg read_ready, write_ready, write_s;
+reg read_ready, write_s;
+//reg write_ready;
 reg [15:0] writedata_left, writedata_right;
 reg [15:0] readdata_left, readdata_right;	
 wire reset, read_s;
@@ -34,5 +35,150 @@ flash flash_inst(.clk_clk(clk), .reset_reset_n(rst_n), .flash_mem_write(1'b0), .
                  .flash_mem_readdata(flash_mem_readdata), .flash_mem_readdatavalid(flash_mem_readdatavalid), .flash_mem_byteenable(flash_mem_byteenable), .flash_mem_writedata());
 
 // your code for the rest of this task here
+
+// an enumerated type for our state machine
+
+typedef enum { state_wait_until_ready, state_store, state_count, state_send_sample, state_wait_for_accepted } state_type;
+
+// some signals I will use in my always block
+
+integer cnt;
+state_type state;
+reg [15:0] sample;
+assign reset = ~(KEY[3]);
+assign read_s = 1'b0;
+
+reg signed [15:0] divisor = 64;
+
+
+//flash reader stuff
+
+int counter;
+
+assign clk = CLOCK_50;
+assign rst_n = KEY[3];
+
+reg signed [31:0] readdata;
+
+logic [7:0] address;
+logic [15:0] data, q;
+logic wren;
+
+logic loc;
+
+assign flash_mem_byteenable = 4'b1111;
+
+
+
+	
+always_ff @(posedge CLOCK_50)
+   if (reset == 1'b1) begin
+         state <= state_wait_until_ready;
+         write_s <= 1'b0;
+	loc <= 0;
+			
+   end else begin
+      case (state)
+		
+      		state_wait_until_ready: begin
+			write_s <= 1'b0;
+                	if (write_ready == 1)  begin
+				if (loc == 0) state <= state_count;
+				else state <= state_send_sample;
+			end
+             	end		   
+   
+            	state_store: 
+            	begin
+			readdata <= flash_mem_readdata;
+			state <= state_send_sample;
+           	end
+
+		state_count:
+		begin
+			if (counter < 24'h80000)
+			begin
+				state <= state_store;
+			end else begin
+				counter <= 0;
+			end
+		end
+
+         	state_send_sample: begin
+					 
+			if (loc == 0) begin
+				writedata_right <= readdata[31:16]/divisor;
+				writedata_left <= readdata[31:16]/divisor;
+			end
+			else begin
+				writedata_right <= readdata[15:0]/divisor;
+				writedata_left <= readdata[15:0]/divisor;
+ 			end
+
+	 		write_s <= 1'b1;  
+
+			loc <= loc + 1;
+			counter <= counter + 1;
+        	        state <= state_wait_for_accepted;
+		end 
+					
+		state_wait_for_accepted: begin
+					 
+			if (write_ready == 1'b0) state <= state_wait_until_ready;    
+			else state <= state_wait_for_accepted;
+		end 
+					
+		default: begin
+				 
+        	        state <= state_wait_until_ready;
+					 
+		end // default
+
+	endcase
+
+end  // if 
+
+always_comb 
+
+begin
+
+	casez(state)
+		state_wait_until_ready: 
+		begin
+			flash_mem_read = 1'b0;
+		end
+
+		state_count:
+		begin
+			flash_mem_read = 1'b1;
+
+			flash_mem_address = counter;
+
+		end
+
+		state_store:
+		begin
+			flash_mem_read = 1'b0;
+
+		end
+
+         	state_send_sample: begin
+
+			flash_mem_read = 1'b0;
+					 
+		end 
+					
+		state_wait_for_accepted: begin
+
+			flash_mem_read = 1'b0;
+
+		end
+
+		default: 
+		begin
+			flash_mem_read = 1'b0;
+		end
+	endcase
+end
 
 endmodule: music
